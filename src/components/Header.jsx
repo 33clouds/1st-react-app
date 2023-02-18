0031aa7ea935395e260d4330ea2c6641b2022c9c
Olga Grigoryeva <33clouds@gmail.com>
2023-02-18 22:14:16 +0100

    fix logo size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import './Header.module.css';
+import s from './Header.module.css';
 
 const Header = () => {
   return (
-  <header className='header'>
+  <header className={s.header}>
     <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/mr-bread-logo-design-template-8a8bf7264deccf537f8dd318785ebd6f_screen.jpg"></img>
   </header>
   );
