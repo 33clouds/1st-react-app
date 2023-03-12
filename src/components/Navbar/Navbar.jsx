bb9a3a1f38c8d537fcdb79d953bb0d8eb9f2d3ab
Olga Grigoryeva <33clouds@gmail.com>
2023-03-12 18:47:28 +0100

    Navigation w/o reload via NavLink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,38 @@
 import classes from './Navbar.module.css';
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className={classes.nav}>
-    <div className={classes.item}>
-      <a href='/profile'>Profile</a>
-    </div>
-    <div className={`${classes.item} ${classes.active}`}>
-      <a href='/dialogs'>Messages</a>
-    </div>
-    <div className={classes.item}>
-      <a href='/news'>News</a>
-    </div>
-    <div className={classes.item}>
-      <a href='/music'>Music</a>
-    </div>
-    <div className={classes.item}>
-      <a href='/settings'>Settings</a>
-    </div>
-  </nav>
+      <div className={classes.item}>
+        <MyNavLink to='/profile'>Profile</MyNavLink>
+      </div>
+      <div className={`${classes.item} ${classes.active}`}>
+        <MyNavLink to='/dialogs'>Messages</MyNavLink>
+      </div>
+      <div className={classes.item}>
+        <MyNavLink to='/news'>News</MyNavLink>
+      </div>
+      <div className={classes.item}>
+        <MyNavLink to='/music'>Music</MyNavLink>
+      </div>
+      <div className={classes.item}>
+        <MyNavLink to='/settings'>Settings</MyNavLink>
+      </div>
+    </nav>
+  );
+}
+
+const MyNavLink = (props) => {
+  return (
+    <NavLink
+      to={props.to}
+      className={({ isActive, isPending }) =>
+        isPending ? classes.pending : isActive ? classes.active : ""
+      }
+    >
+    {props.children}
+    </NavLink>
   );
 }
 
