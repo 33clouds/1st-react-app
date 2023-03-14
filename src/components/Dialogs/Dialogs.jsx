f0fb1203fec44716b6aaf62f8530810c1a92cd70
Olga Grigoryeva <33clouds@gmail.com>
2023-03-14 23:27:43 +0100

    Dialogs page enhancement

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,13 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
   return (
-    <div >
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogItems}>
+        <div className={s.dialog + ' ' + s.active}> Olga </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}> Hi </div>
+      </div>
     </div>
   );
 }
